b409cae0cab36af8819ba2e6d1d180c22425ae6d
Eduardo Balbino Valença <valencabalbino@hotmail.com>
2025-07-15 15:40:03 -0300

    treinos de lógica

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Nada();
+        List<string> Users = new List<string>();
+        Users.Add("Ribamar_Vascao");
+        Users.Add("Ribamar_Vascao");
+        Users.Add("Ribamar_Vascao");
+
+        foreach (string User in Users)
+        {
+            Console.WriteLine(User);
+        }
+
+    }
+    public static void Nada()
+    {
+        Console.WriteLine("teste");
+    }
+}
